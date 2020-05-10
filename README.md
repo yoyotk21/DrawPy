42fa6e0b1adfcb8d1e54d477e5decc6d77675787
Joel Kalai <yoyot@Yael-SurfaceLa2.northamerica.corp.microsoft.com>
2020-05-09 21:43:29 -0400

    updating setup files

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # DrawPy
-A game and graphics library built on pygame
+Hello and welcome to DrawPy!
+This is a simple graphics package for python based on pygame, so you need pygame to run this.
+This package is meant for graphics and basic games and GUI's.
+Here is a little example to get you started. For more, run example_drawing() or example_animation():
+    
+    from DrawPy import *
+
+
+    def main():
+        screen = Window((500, 500), "MyWindow")  # Creates the window
+        screen.circle(BLACK, [250, 250], 250, 0)  # Draws a circle centered at (250, 250) with a radius of 250
+        draw()  # Flips the drawings on to the screen
+        done()  # Waits to close the window
+
+
+    main()
+-----------------------------------------------------------
+run the help() function for more help
+Please Enjoy!
--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ setup(
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A simple game and graphics library built on pygame',   # Give a short description about your library
   author = 'Joel Kalai',                   # Type in your name
-  author_email = 'yoyotk21@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  author_email = 'jalapenoinyoureye@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/yoyotk21/DrawPy',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/yoyotk21/DrawPy/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['game', 'graphics', 'simple'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'pygame'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
