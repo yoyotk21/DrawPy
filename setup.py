042eba561ef4fb52632d0dd3a28ec289aaab45e0
yoyotk21 <64795830+yoyotk21@users.noreply.github.com>
2020-05-15 10:02:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='DrawPy',  # How you named your package folder (MyLib)
     packages=['DrawPy'],  # Chose the same as "name"
-    version='0.3',  # Start with a small number and increase it with every change you make
+    version='0.4',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='A simple game and graphics library built on pygame',  # Give a short description about your library
     author='Joel Kalai',  # Type in your name
