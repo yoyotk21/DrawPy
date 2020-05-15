afae2ce922831a5b2e8ffa10dce525cc0cfba933
yoyotk21 <64795830+yoyotk21@users.noreply.github.com>
2020-05-15 10:16:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Joel Kalai',  # Type in your name
     author_email='jalapenoinyoureye@gmail.com',  # Type in your E-Mail
     url='https://github.com/yoyotk21/DrawPy',  # Provide either the link to your github or to your website
-    download_url='https://github.com/yoyotk21/DrawPy/archive/0.3.tar.gz',  # I explain this later on
+    download_url='https://github.com/yoyotk21/DrawPy/archive/0.4.tar.gz',  # I explain this later on
     keywords=['game', 'graphics', 'simple'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'pygame'
